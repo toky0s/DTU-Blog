f821e4c0f5fb69389b3d9dd9269b53a77fb0cc72
Thorin Oakenshield <thror1200@gmail.com>
2021-03-16 18:26:11 +0700

    api idea

--- fake_DB.js
+++ fake_DB.js
@@ -1,14 +1,46 @@
-const api = [
-  {
-    id: "2d4d3078-1859-5b53-97ec-4657c9348ee0",
-    author: "Lọ lem",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    tag: ["#alo"],
-    comment: [],
-    react: [],
-    initTime: Date.getDate(),
-  },
-];
+const api = {
+  post: [
+    {
+      id: "2d4d3078-1859-5b53-97ec-4657c9348ee0",
+      author: "Lọ lem",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      tag: ["#alo"],
+      comment: [],
+      react: [],
+      initTime: Date.getDate(),
+    },
+    {
+      id: "2d4d3078-1859-5b53-97ec-4657c9348ee0",
+      author: "Lọ lem",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      tag: ["#alo"],
+      comment: [],
+      react: [],
+      initTime: Date.getDate(),
+    },
+    {
+      id: "2d4d3078-1859-5b53-97ec-4657c9348ee0",
+      author: "Con cặc",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      tag: ["#blo"],
+      comment: [],
+      react: [],
+      initTime: Date.getDate(),
+    },
+    {
+      id: "2d4d3078-1859-5b53-97ec-4657c9348ee0",
+      author: "yasuo",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      tag: ["#clo"],
+      comment: [],
+      react: [],
+      initTime: Date.getDate(),
+    },
+  ],
+};
 
 export default api;
